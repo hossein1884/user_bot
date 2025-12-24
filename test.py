75101f5a205e58db0eb8767cd7d28a57236e6367
hossein <hosseinhosseinzade1383@gmail.com>
2025-12-24 14:30:07 +0330

    test deleted

--- test.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-cn=sqlite3.connect("data.db")
-cur=cn.cursor()
-result=cn.execute("select 1 from users where id=?",(176095541,)).fetchone())
-# print(cn.execute("select * from users").fetchone())
